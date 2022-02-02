321cab3ee32dbfd456c27682b93d19c8b841907f
Adam Farley <farley13@gmail.com>
2022-02-02 03:39:57 -0500

    Updating to working playjs react + typescript config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.css';
+import logo from '../public/logo.png'; 
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {	
+  render() {
+    interface User {
+  id: number
+  firstName: string
+  lastName: string
+  role: string
 }
+    
+    const user:User = {
+  firstName: "Angela",
+  lastName: "Davis",
+  role: "Professor",
+}
+ 
+console.log(user.name)
+    let b:Intbb = "thing"
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          No! need to Edit <code>src/app.jsx</code> and tap.... on the play button.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
